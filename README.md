8efcebc5ce0b9723c58c83806dabefa0175a59cc
Akash Manohar J <akash@akash.im>
2013-09-29 13:59:06 +0530

    Initial commit with umbrella project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elixir Samples
+
+Sample elixir applications
---
+++ mix.exs
@@ -0,0 +1,15 @@
+defmodule ElixirSamples.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ apps_path: "apps",
+      deps: deps ]
+  end
+
+  # Returns the list of dependencies in the format:
+  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
+  # These dependencies are not accessible from child applications
+  defp deps do
+    []
+  end
+end
